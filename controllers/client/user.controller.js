a22404511ca97ddbda05935e62b47cf0e8d79c8a
longdn81 <146925221+longdn81@users.noreply.github.com>
2024-05-24 02:15:31 +0700

    update register , login ,logout

---
+++ controllers/client/user.controller.js
@@ -0,0 +1,119 @@
+const md5 = require("md5")
+const User = require("../../models/user.model")
+const ForgotPassword = require("../../models/forgot-password.model")
+
+const generateHelper =require("../../helpers/generate")
+// [GET] /user/register
+module.exports.register = async (req, res) => {
+    res.render("client/pages/user/register" ,{
+        pageTitle : "Dang ki tai khoan",
+        
+    });
+}
+
+// [POST] /user/register
+module.exports.registerPost = async (req, res) => {
+    const  existEmail = await User.findOne({
+        email : req.body.email ,
+    })
+
+    if(existEmail){
+        req.flash("error","Email da ton tai!")
+        res.redirect("back")
+        return ;
+    }
+
+    req.body.password = md5(req.body.password);
+
+    const user = new User(req.body);
+    await user.save();
+
+    res.cookie("tokenUser" , user.tokenUser)
+
+    res.redirect("/")
+}
+
+// [GET] /user/login
+module.exports.login = async (req, res) => {
+    res.render("client/pages/user/login" ,{
+        pageTitle : "Đăng nhập tài khoản",
+        
+    });
+}
+
+// [POST] /user/login
+module.exports.loginPost = async (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    const user = await User.findOne({ 
+        email: email,
+        deleted: false
+    });
+
+    if(!user) { 
+        req.flash("error", "Email không tồn tại!"); 
+        res.redirect("back"); 
+        return; 
+    }
+
+    if(md5(password) !== user.password) { 
+        req.flash("error", "Sai mật khẩu!");
+        res.redirect("back"); 
+        return;
+
+    }
+
+    if(user.status === "inactive") { 
+        req.flash("error", "Tài khoản đang bị khóa!");
+        res.redirect("back"); 
+        return;
+    }
+
+    res.cookie("tokenUser",user.tokenUser);
+    
+    res.redirect("/")
+}
+
+// [GET] /user/logout
+module.exports.logout = async (req, res) => {
+    res.clearCookie("tokenUser");
+    res.redirect("/")
+}
+
+// [GET] /user/password/forgot
+module.exports.forgotPassword = async (req, res) => {
+    res.render("client/pages/user/forgot-password" ,{
+        pageTitle : "Lấy lại mật khẩu",
+        
+    });
+}
+
+// [POST] /user/password/forgot
+module.exports.forgotPasswordPost = async (req, res) => {
+    const email = req.body.email;
+
+    const user = await User.findOne({
+        email: email, 
+        deleted: false 
+    });
+
+    if(!user) { 
+        req.flash("error", "Email không tồn tại!"); 
+        res.redirect("back"); 
+        return; 
+    }
+    //Lưu thông tin vào Db
+    const otp = generateHelper.generateRandomNumber(8);
+    
+    const objectForgotPassword = {
+        email : email ,
+        otp : otp ,
+        expireAt : Date.now(),
+    }
+
+    const forgotPassword = new ForgotPassword(objectForgotPassword);
+    await forgotPassword.save();
+    // Nếu tồn tại email thì gửi mã OTP qua email
+    res.send("ok")
+}
--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,16 +1,20 @@
 const categoryMiddleware = require("../../middlewares/client/category.middleware")
 const cartMiddleware = require("../../middlewares/client/cart.middleware")
+const userMiddleware = require("../../middlewares/client/user.middleware")
+
 
 const productRoutes = require("./products.route")
 const homeRoutes = require("./home.route")
 const searchRoutes = require("./search.route")
 const cartRoutes = require("./cart.route")
 const checkoutRoutes = require("./checkout.route")
+const userRoutes = require("./user.route")
 
 
 module.exports = (app) => {
     app.use(categoryMiddleware.category);
     app.use(cartMiddleware.cartId);
+    app.use(userMiddleware.infoUser);
 
     app.use('/', homeRoutes);
     
@@ -21,6 +25,8 @@ module.exports = (app) => {
     app.use('/cart', cartRoutes );
 
     app.use('/checkout', checkoutRoutes );
+    
+    app.use('/user', userRoutes );
 
 
 }
